8ad044437bd8bc4c235dfc80192eb80543600c1d
Λεωνίδας Στεφανίδης <leonidas@ambrosiadi.local>
2018-07-10 15:52:23 +0300

    Layout

--- MVCCoreApp/Controllers/HomeController.cs
+++ MVCCoreApp/Controllers/HomeController.cs
@@ -10,24 +10,18 @@ namespace MVCCoreApp.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
 
+        public IActionResult Things()
+        {
             return View();
         }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
+               
 
         public IActionResult Error()
         {
--- README.md
+++ README.md
@@ -6,3 +6,24 @@ Manage field services, schedule and complete jobs and more
 
 * Create an ASP.NET Core Web Application, use the MVC scaffold.
 
+
+## 01 Layout
+
+* Edit the main layout.
+
+`Views/Shared/_Layout.cshtml`
+
+```
+<a asp-area="" asp-controller="Home" asp-action="Index" class="navbar-brand">MVC Core App</a>
+```
+
+```
+<li><a asp-area="" asp-controller="Home" asp-action="Things">Things</a></li>
+```
+
+* Add the Things  action stub in Controllers/HomeController.
+
+
+* Add the Things cshtml stub in Views/Home.
+
+
